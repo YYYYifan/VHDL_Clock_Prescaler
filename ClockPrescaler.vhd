d2e003f53765029faa2ae73ffd2d3fb56746cec2
Mr.Mario <Mr.Mario@users.noreply.github.com>
2020-04-21 01:42:10 +0100

    Update ClockPrescaler.vhd

--- ClockPrescaler.vhd
+++ ClockPrescaler.vhd
@@ -13,17 +13,17 @@ end ClockPrecaler;
 
 architecture Behavioral of ClockPrecaler is
     signal prescaler : std_logic_vector (27 downto 0);
-    signal clk_2Hz_i : std_logic;
+    signal clk_1Hz_i : std_logic;
 begin
     gen_clk : process (CLK100MHZ, rst)
     begin  -- process gen_clk
         if rst = '1' then
-            clk_2Hz_i   <= '0';
+            clk_1Hz_i   <= '0';
             prescaler   <= (others => '0');
-        elsif rising_edge(CLK100MHZ) then   -- rising clock edge
-            if prescaler = X"2faf080" then     -- 17d7840 <-> 25 000 000 in hex ! we need 50 hz
+        elsif rising_edge(CLK100MHZ) then   
+            if prescaler = X"2faf080" then      -- change here to change clock, 5000 00 000 in hex is 2faf080
                 prescaler   <= (others => '0');
-                clk_2Hz_i   <= not clk_2Hz_i;
+                clk_1Hz_i   <= not clk_1Hz_i;
             else
                 prescaler <= prescaler + "1";
             end if;
@@ -31,5 +31,5 @@ begin
     end process gen_clk;
       
     LEDS(7 downto 1) <= (others => '0');
-    LEDS(0) <= clk_2Hz_i;
+    LEDS(0) <= clk_1Hz_i;
 end Behavioral;
